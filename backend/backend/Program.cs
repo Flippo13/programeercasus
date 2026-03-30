aec4eb6a18c04fe11e43a366e356cc03f98ae253
Felix de Natris <felixdenatris@hotmail.nl>
2026-03-30 23:55:51 +0200

    initialized .net project

---
+++ backend/backend/Program.cs
@@ -0,0 +1,7 @@
+﻿var builder = WebApplication.CreateBuilder(args); 
+var app = builder.Build(); 
+
+// See https://aka.ms/new-console-template for more information
+app.MapGet("/", () -> "Hello World");
+
+app.Run(); 
